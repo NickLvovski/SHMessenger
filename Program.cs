81f7c096c271c7941597bedc48ad2bfc66a2aa0a
NickLvovski <nicklvovski@gmail.com>
2022-01-22 02:13:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SHMessenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
