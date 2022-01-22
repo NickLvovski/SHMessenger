7fdb6813d54846aba30e844607f1a3507aeacddd
NickLvovski <nicklvovski@gmail.com>
2022-01-22 19:08:39 +0300

    Revert "Revert "Добавлена сериализация сообщений.""
    This reverts commit 10935b19c414db0631381b57b632f9db904b7e46.

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace SHMessenger
 {
-    partial class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            var msg = new Message();
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("Shnucker", "Hi", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"Shnucker","MessageText":"Hi","TimeStamp":"2022-01-22T15:36:30.2664146Z"}
+            //Shnucker < 22.01.2022 15:36:30 >: Hi
         }
     }
 }
